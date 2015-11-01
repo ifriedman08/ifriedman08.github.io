821e38d373af5d44573841b0253eaa7ef5318234
ifriedman08 <i.friedman.08@gmail.com>
2015-11-01 13:01:27 -0800

    minor edits

--- pebble.js
+++ pebble.js
@@ -1,4 +1,4 @@
-// simply.title('Travel time:');
+simply.title('Travel time:');
 
 
 var locationOptions = {
@@ -8,25 +8,44 @@ var locationOptions = {
 };
 
 
-// function locationSuccess(pos) {
-//   console.log('Location Success');
-//   ajax({
-//     'url': 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + pos.coords.latitude + ',' + pos.coords.longitude + '&destinations=San+Jose',
-//     'type': 'json'
-//   },
-//   function (response) {
-//     console.log('traff obj loaded');
-//     simply.body('Work:' + response.rows[0].elements[0].duration.text);
-//   });
-// }
-//
-//
-// function locationError(err) {
-//   console.log('Location Error');
-//   console.log('location error (' + err.code + '): ' + err.message);
-// }
-//
-// navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+function locationSuccess(pos) {
+  console.log('Location Success');
+  ajax({
+    'url': 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + pos.coords.latitude + ',' + pos.coords.longitude + '&destinations=37.412517,-121.937146',
+    'type': 'json'
+  },
+  function (response) {
+    console.log('work loc obj loaded');
+    work_str = 'Work:' + response.rows[0].elements[0].duration.text;
+  });
 
-simply.text({ title: 'A1', subtitle: 'B1', body: 'C1'})
-simply.text({ title: 'A2', subtitle: 'B2', body: 'C2'})
+  ajax({
+    'url': 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + pos.coords.latitude + ',' + pos.coords.longitude + '&destinations=37.852157,-122.288795',
+    'type': 'json'
+  },
+  function (response) {
+    console.log('berk loc obj loaded');
+    berk_str = 'Berkeley:' + response.rows[0].elements[0].duration.text;
+  });
+
+  ajax({
+    'url': 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + pos.coords.latitude + ',' + pos.coords.longitude + '&destinations=37.8132267,-122.2480338',
+    'type': 'json'
+  },
+  function (response) {
+    console.log('Oakland obj loaded');
+    oak_str = 'Oakland:' + response.rows[0].elements[0].duration.text;
+  });
+
+  simply.body('Work: ' + work_str + '\n'
+            + 'Berkeley: ' + berk_str + '\n'
+            + 'Oakland: ' + oak_str)
+}
+
+
+function locationError(err) {
+  console.log('Location Error');
+  console.log('location error (' + err.code + '): ' + err.message);
+}
+
+navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
