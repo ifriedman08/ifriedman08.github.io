68b8aec1ed243659c80848bb8ee4723d2de63705
ifriedman08 <i.friedman.08@gmail.com>
2015-10-31 17:04:55 -0700

    update

--- pebble.js
+++ pebble.js
@@ -1,4 +1,4 @@
 console.log('running pebble.js');
 simply.title('Hello World!');
-simply.title('Hello World!');
-simply.title('Hello World!');
+simply.body('Helwdcnlbbsdkhfbvkshdvbdfhk');
+// simply.title('Hello World!');
