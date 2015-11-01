1471da00414c27f1f17452c2df8fd356dca8e588
ifriedman08 <i.friedman.08@gmail.com>
2015-10-31 17:31:22 -0700

    edits

--- pebble.js
+++ pebble.js
@@ -3,6 +3,13 @@ console.log('running pebble.js');
 simply.title('Hello Nathalie!');
 simply.body('I like you.');
 
-var currPos = navigator.geolocation.getCurrentPosition();
+// var currPos = navigator.geolocation.getCurrentPosition();
 
 // console.log(JSON.stringify(currPos));
+navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+console.log();
