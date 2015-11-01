25022642bc03db8e4b02757a96a216fffafb1899
ifriedman08 <i.friedman.08@gmail.com>
2015-11-01 12:23:15 -0800

    minor edits

--- pebble.js
+++ pebble.js
@@ -11,9 +11,14 @@ var locationOptions = {
 function locationSuccess(pos) {
   console.log('Location Success');
   simply.body('lat= ' + pos.coords.latitude + ' lon= ' + pos.coords.longitude);
-  traff_to_work_obj= require('https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + pos.coords.latitude + ',' + pos.coords.longitude + '&destinations=San+Jose');
-  console.log('traff obj loaded');
-  console.log(traff_to_work_obj);
+  traff_to_work_obj= ajax({
+    'url': 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + pos.coords.latitude + ',' + pos.coords.longitude + '&destinations=San+Jose',
+    'type': 'json'
+  },
+  function () {
+    console.log('traff obj loaded');
+    console.log(traff_to_work_obj);    
+  });
 
 
   // var imported = document.createElement('script');
