56232cf96d99b438121d890c75f837cd9ce98c35
ifriedman08 <i.friedman.08@gmail.com>
2015-11-01 21:33:55 -0800

    minor edits

--- pebble.js
+++ pebble.js
@@ -1,5 +1,8 @@
 simply.title('Travel time:');
 
+load_count = 0;
+
+simply.body('Loading...')
 
 var locationOptions = {
   enableHighAccuracy: true,
@@ -9,40 +12,51 @@ var locationOptions = {
 
 final_body = '';
 
-
 function locationSuccess(pos) {
   console.log('Location Success');
   ajax({
     'url': 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + pos.coords.latitude + ',' + pos.coords.longitude + '&destinations=37.412517,-121.937146',
     'type': 'json'
   },
+
   function (response) {
-    console.log('work loc obj loaded');
+    load_count += 1;
     work_str = 'Work: ' + response.rows[0].elements[0].duration.text;
     final_body += work_str + '\n';
     simply.body(final_body);
+    if (load_count == 3) {
+      simple.vibe();
+    };
   });
 
   ajax({
     'url': 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + pos.coords.latitude + ',' + pos.coords.longitude + '&destinations=37.852157,-122.288795',
     'type': 'json'
   },
+
   function (response) {
-    console.log('berk loc obj loaded');
+    load_count += 1;
     berk_str = 'Berkeley: ' + response.rows[0].elements[0].duration.text;
     final_body += berk_str + '\n';
     simply.body(final_body);
+    if (load_count == 3) {
+      simple.vibe();
+    };
   });
 
   ajax({
     'url': 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + pos.coords.latitude + ',' + pos.coords.longitude + '&destinations=37.8132267,-122.2480338',
     'type': 'json'
   },
+
   function (response) {
-    console.log('Oakland obj loaded');
+    load_count += 1;
     oak_str = 'Oakland: ' + response.rows[0].elements[0].duration.text;
     final_body += oak_str + '\n';
     simply.body(final_body);
+    if (load_count == 3) {
+      simple.vibe();
+    };
   });
 
 }
